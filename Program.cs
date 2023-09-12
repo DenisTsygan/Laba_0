25787c2fa08260816dfe60307579caaf5917c94b
DenisTsygan <denis.www.denis7@gmail.com>
2023-09-12 15:24:06 +0300

    add function CubingDiferenceOfTwoNumbers

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace Laba_0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestFunction();
         }
+        /// <summary>
+        /// Тестирование на роботоспособность и правильность выполнения функции CubingDiferenceOfTwoNumbers
+        /// </summary>
+        private static void TestFunction()
+        {
+            int firstNumber = GenerateRandomValue();
+            int secondNumber = GenerateRandomValue();
+            double result = CubingDiferenceOfTwoNumbers(firstNumber, secondNumber);
+            Console.WriteLine($"({firstNumber} - {secondNumber})^3 = {result} ");
+        }
+        /// <summary>
+        /// Генерация рандомного числа
+        /// </summary>
+        /// <returns></returns>
+        private static int GenerateRandomValue()
+        {
+            Random random = new ();
+            int minValue = -100;
+            int maxValue = 100;
+            int rundomNumber = random.Next(minValue, maxValue);
+            return rundomNumber;
+        }
+        /// <summary>
+        /// Возведения в куб разности двух целых чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static double CubingDiferenceOfTwoNumbers(int a,int b)
+        {
+            return Math.Pow((a - b), 3);
+        }
+        
     }
 }
